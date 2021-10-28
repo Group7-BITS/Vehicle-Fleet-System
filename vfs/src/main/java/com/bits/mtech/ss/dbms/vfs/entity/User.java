5031753cf79b3d2b717b9300c088a98b246ec4c2
snsainiyk1 <93337134+snsainiyk1@users.noreply.github.com>
2021-10-28 22:13:23 +0530

    Update User.java

--- vfs/src/main/java/com/bits/mtech/ss/dbms/vfs/entity/User.java
+++ vfs/src/main/java/com/bits/mtech/ss/dbms/vfs/entity/User.java
@@ -7,7 +7,7 @@ import javax.persistence.Id;
 @Entity
 public class User {
 	/**
-	 * this file is updated by yogesh
+	 * this file is updated by yogesh. don't modigy this
 	 */
 	private @Id @GeneratedValue Long id;
 	
