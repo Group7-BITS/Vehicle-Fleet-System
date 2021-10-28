2ba8bbbe59df609c258d736109c017f010b31624
ashrithshettybits <2021mt12192@wilp.bits-pilani.ac.in>
2021-10-28 22:00:25 +0530

    Updated Address Entity

--- vfs/src/main/java/com/bits/mtech/ss/dbms/vfs/entity/Address.java
+++ vfs/src/main/java/com/bits/mtech/ss/dbms/vfs/entity/Address.java
@@ -4,6 +4,10 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
 
+/**
+ * This file is updated by Ashrith Shetty. Do not modify this file.
+ */
+
 @Entity
 public class Address {
 	private @Id @GeneratedValue Long id;
