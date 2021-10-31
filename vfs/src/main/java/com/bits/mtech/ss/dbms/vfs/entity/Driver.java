e9299cbf5192c5d00b82fa29588ed302132758bb
ashrithshettybits <2021mt12192@wilp.bits-pilani.ac.in>
2021-10-31 14:49:32 +0530

    Updating the variable type

--- vfs/src/main/java/com/bits/mtech/ss/dbms/vfs/entity/Driver.java
+++ vfs/src/main/java/com/bits/mtech/ss/dbms/vfs/entity/Driver.java
@@ -1,4 +1,6 @@
 package com.bits.mtech.ss.dbms.vfs.entity;
+import java.util.Date;
+
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
@@ -15,16 +17,16 @@ public class Driver {
 	private @Id @GeneratedValue Long id;
 	
 	@Column(name = "DRIVER_LICENSE")
-	private String driverlicense;
+	private Date driverlicense;
 
 	@Column(name = "LICENSE_EXPIRY")
-	private String licenseExpiry;
+	private Date licenseExpiry;
 
 	@Column(name = "LICENSE_TYPE")
 	private String licenseType;
 	
 	
-	public Driver(String driverlicense, String licenseExpiry, String licenseType) {
+	public Driver(Date driverlicense, Date licenseExpiry, String licenseType) {
 		super();
 		this.driverlicense = driverlicense;
 		this.licenseExpiry = licenseExpiry;
@@ -39,19 +41,19 @@ public class Driver {
 		this.id = id;
 	}
 
-	public String getDriverlicense() {
+	public Date getDriverlicense() {
 		return driverlicense;
 	}
 
-	public void setDriverlicense(String driverlicense) {
+	public void setDriverlicense(Date driverlicense) {
 		this.driverlicense = driverlicense;
 	}
 
-	public String getLicenseExpiry() {
+	public Date getLicenseExpiry() {
 		return licenseExpiry;
 	}
 
-	public void setLicenseExpiry(String licenseExpiry) {
+	public void setLicenseExpiry(Date licenseExpiry) {
 		this.licenseExpiry = licenseExpiry;
 	}
 
