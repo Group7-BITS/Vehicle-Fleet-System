24f69eaf287b8fd9488faa1df5ca25d4ef2f55aa
snsainiyk1 <93337134+snsainiyk1@users.noreply.github.com>
2021-10-31 15:01:50 +0530

    Update UserRole.java

--- vfs/src/main/java/com/bits/mtech/ss/dbms/vfs/entity/UserRole.java
+++ vfs/src/main/java/com/bits/mtech/ss/dbms/vfs/entity/UserRole.java
@@ -1,9 +1,54 @@
 package com.bits.mtech.ss.dbms.vfs.entity;
+//import java.util.Date;
+
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
+import javax.persistence.Table;
 
 @Entity
+@Table(name = "UserRole")
 public class UserRole {
 	private @Id @GeneratedValue Long id;
+	/**
+	 * this file is updated by yogesh. please don't modigy this
+	 */
+	@Column(name = "USER_ROLE")
+	private String userRole;
+
+	public UserRole(String userRole) {
+		super();
+		this.userRole = userRole;
+
+	}
+
+	
+
+	public UserRole() {
+		super();
+	}
+
+
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getUserRole() {
+		return userRole;
+	}
+
+	public void getUserRole(String userRole) {
+		this.userRole = userRole;
+	}
+
+	
+	
+	
+	
 }
