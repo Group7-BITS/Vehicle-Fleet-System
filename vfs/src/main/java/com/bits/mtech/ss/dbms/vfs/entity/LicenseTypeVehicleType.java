673027f9dc0c93d8f7d308637ac538b859228948
SreeHarshithaMandapaka <2021mt12067@wilp.bits-pilani.ac.in>
2021-10-31 18:46:09 +0530

    ++the changes to "triproute" and "LicenseTypeVehicleType" were committed by SreeHarshitha

--- vfs/src/main/java/com/bits/mtech/ss/dbms/vfs/entity/LicenseTypeVehicleType.java
+++ vfs/src/main/java/com/bits/mtech/ss/dbms/vfs/entity/LicenseTypeVehicleType.java
@@ -1,9 +1,43 @@
 package com.bits.mtech.ss.dbms.vfs.entity;
+import java.sql.NClob;
+
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
-
+//code updated by SreeHarshithaMandapaka
 @Entity
 public class LicenseTypeVehicleType {
 	private @Id @GeneratedValue Long id;
+	@Column(name = "TYPECODE")
+	private String TypeCode;
+	@Column(name = "CAPACITY")
+	private String capacity;
+	public Long getId() {
+		return id;
+	}
+	public void setId(Long id) {
+		this.id = id;
+	}
+	public String getTypeCode() {
+		return TypeCode;
+	}
+	public void setTypeCode(String typeCode) {
+		TypeCode = typeCode;
+	}
+	public String getCapacity() {
+		return capacity;
+	}
+	public void setCapacity(String capacity) {
+		this.capacity = capacity;
+	}
+	public LicenseTypeVehicleType(Long id, String TypeCode, String capacity) {
+		super();
+		this.id=id;
+		this.TypeCode=TypeCode;
+		this.capacity=capacity;
+		
+		
+	}	
+	
 }
--- vfs/src/main/java/com/bits/mtech/ss/dbms/vfs/entity/TripRoute.java
+++ vfs/src/main/java/com/bits/mtech/ss/dbms/vfs/entity/TripRoute.java
@@ -1,9 +1,36 @@
 package com.bits.mtech.ss.dbms.vfs.entity;
+import java.sql.NClob;
+
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
-
+import javax.persistence.Table;
+//code updated by SreeHarshithaMandapaka
 @Entity
+@Table(name = "TripRoute")
 public class TripRoute {
 	private @Id @GeneratedValue Long id;
+	@Column(name = "GPS")
+	private NClob gps;
+	
+	public Long getId() {
+		return id;
+	}
+	public void setId(Long id) {
+		this.id = id;
+	}
+	public NClob gps() {
+		return gps;
+	}
+	public void gps(NClob gps) {
+		this.gps = gps;
+	}
+	public TripRoute(Long id, NClob gps) {
+		super();
+		this.id=id;
+		this.gps=gps;
+		
+		
+	}	
 }
