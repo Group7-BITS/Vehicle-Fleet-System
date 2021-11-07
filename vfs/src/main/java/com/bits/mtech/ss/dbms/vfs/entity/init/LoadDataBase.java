64df9c19451e83160c9c5d9019bec322702a2c07
snsainiyk1 <93337134+snsainiyk1@users.noreply.github.com>
2021-11-07 18:45:23 +0530

    Update LoadDataBase.java

--- vfs/src/main/java/com/bits/mtech/ss/dbms/vfs/entity/init/LoadDataBase.java
+++ vfs/src/main/java/com/bits/mtech/ss/dbms/vfs/entity/init/LoadDataBase.java
@@ -10,6 +10,10 @@ import org.springframework.context.annotation.Configuration;
 
 import com.bits.mtech.ss.dbms.vfs.entity.Vehicle;
 import com.bits.mtech.ss.dbms.vfs.entity.repository.VehicleRepository;
+import com.bits.mtech.ss.dbms.vfs.entity.User;
+import com.bits.mtech.ss.dbms.vfs.entity.repository.UserRepository;
+import com.bits.mtech.ss.dbms.vfs.entity.UserRole;
+import com.bits.mtech.ss.dbms.vfs.entity.repository.UserRoleRepository;
 
 @Configuration
 class LoadDatabase {
@@ -17,11 +21,29 @@ class LoadDatabase {
   private static final Logger log = LoggerFactory.getLogger(LoadDatabase.class);
 
   @Bean
-  CommandLineRunner initDatabase(VehicleRepository vehicleRepository) {
+  CommandLineRunner initDatabase(VehicleRepository vehicleRepository,UserRepository userRepository, UserRoleRepository userRoleRepository) {
 
     return args -> {
       log.info("Preloading {}", vehicleRepository.save(new Vehicle("CH-0567899", "RG-UP074567", new Date())));
       log.info("Preloading {}" ,vehicleRepository.save(new Vehicle("CH-879763536", "RG-TG089765", new Date())));
+      log.info("Preloading {}", userRepository.save(new User("yogesh", "saini", "yk@gmail.com","7566004312","1")));
+      log.info("Preloading {}" ,userRepository.save(new User("yks", "saini", "yk@gmail.com","7566004312","2")));
+      log.info("Preloading {}", userRoleRepository.save(new UserRole("Customer")));
+      log.info("Preloading {}" ,userRoleRepository.save(new UserRole("Vendor")));
+      
     };
+    
   }
+//  @Bean
+//  CommandLineRunner initDatabase(UserRepository userRepository) {
+//
+//    return args -> {
+//      log.info("Preloading {}", userRepository.save(new User("yogesh", "saini", "yk@gmail.com","7566004312","1")));
+//      log.info("Preloading {}" ,userRepository.save(new User("yks", "saini", "yk@gmail.com","7566004312","2")));
+//    };
+//    
+//  }
+  
+  
+  
 }
