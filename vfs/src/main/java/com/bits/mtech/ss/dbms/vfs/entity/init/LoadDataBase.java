c5de5fec9853d225bec7862259168cd54cb5153f
snsainiyk1 <93337134+snsainiyk1@users.noreply.github.com>
2021-11-08 08:27:49 +0530

    Update LoadDataBase.java

--- vfs/src/main/java/com/bits/mtech/ss/dbms/vfs/entity/init/LoadDataBase.java
+++ vfs/src/main/java/com/bits/mtech/ss/dbms/vfs/entity/init/LoadDataBase.java
@@ -18,7 +18,15 @@ import com.bits.mtech.ss.dbms.vfs.entity.LicenseTypeVehicleType;
 import com.bits.mtech.ss.dbms.vfs.entity.Trip;
 import com.bits.mtech.ss.dbms.vfs.entity.TripRoute;
 import com.bits.mtech.ss.dbms.vfs.entity.User;
+import com.bits.mtech.ss.dbms.vfs.entity.UserRole;
 import com.bits.mtech.ss.dbms.vfs.entity.UserCredential;
+
+import com.bits.mtech.ss.dbms.vfs.entity.VehicleLocation;
+import com.bits.mtech.ss.dbms.vfs.entity.VehicleProblemType;
+import com.bits.mtech.ss.dbms.vfs.entity.VehicleServices;
+import com.bits.mtech.ss.dbms.vfs.entity.VehicleType;
+import com.bits.mtech.ss.dbms.vfs.entity.VehicleVendor;
+
 import com.bits.mtech.ss.dbms.vfs.entity.repository.AddressRepository;
 import com.bits.mtech.ss.dbms.vfs.entity.repository.BookingRepository;
 import com.bits.mtech.ss.dbms.vfs.entity.repository.DriverRepository;
@@ -27,9 +35,15 @@ import com.bits.mtech.ss.dbms.vfs.entity.repository.TripRepository;
 import com.bits.mtech.ss.dbms.vfs.entity.repository.TripRouteRepository;
 import com.bits.mtech.ss.dbms.vfs.entity.repository.UserCredentialRepository;
 import com.bits.mtech.ss.dbms.vfs.entity.repository.UserRepository;
-import com.bits.mtech.ss.dbms.vfs.entity.UserRole;
+
 import com.bits.mtech.ss.dbms.vfs.entity.repository.UserRoleRepository;
 import com.bits.mtech.ss.dbms.vfs.entity.repository.VehicleInsuranceRepository;
+
+import com.bits.mtech.ss.dbms.vfs.entity.repository.VehicleLocationRepository;
+import com.bits.mtech.ss.dbms.vfs.entity.repository.VehicleProblemTypeRepository;
+import com.bits.mtech.ss.dbms.vfs.entity.repository.VehicleServicesRepository;
+import com.bits.mtech.ss.dbms.vfs.entity.repository.VehicleTypeRepository;
+import com.bits.mtech.ss.dbms.vfs.entity.repository.VehicleVendorRepository;
 
 @Configuration
 class LoadDatabase {
@@ -41,7 +55,9 @@ class LoadDatabase {
 		  AddressRepository addressRepository, BookingRepository bookingRepository, DriverRepository driverRepository,
 		  LicenseTypeVehicleTypeRepository licenseTypeVehicletypeRepository, TripRepository tripRepository, 
 		  TripRouteRepository tripRouteRepository, UserCredentialRepository userCredentialRepository,
-		  VehicleInsuranceRepository vehicleInsuranceRepository) {
+		  VehicleInsuranceRepository vehicleInsuranceRepository, VehicleLocationRepository vehicleLocationRepository,
+		  VehicleProblemTypeRepository vehicleProblemTypeRepository, VehicleServicesRepository vehicleServicesRepository, VehicleTypeRepository vehicleTypeRepository,
+		  VehicleVendorRepository vehicleVendorRepository) {
 
     return args -> {
       log.info("Preloading {}", vehicleRepository.save(new Vehicle("CH-0567899", "RG-UP074567", new Date())));
@@ -58,6 +74,11 @@ class LoadDatabase {
       log.info("Preloading {}" ,tripRouteRepository.save(new TripRoute("41°24’12.2″N   2°10’26.5″E")));
       log.info("Preloading {}" ,userCredentialRepository.save(new UserCredential("iweuryiweurihei9738473dsd", "key")));
       log.info("Preloading {}" ,vehicleInsuranceRepository.save(new VehicleInsurance(new Date(1636311082), new Date(1646311082))));
+      log.info("Preloading {}", vehicleLocationRepository.save(new VehicleLocation("1","41°24’12.2″N   2°10’26.5″E")));
+      log.info("Preloading {}", vehicleProblemTypeRepository.save(new VehicleProblemType("T1","Engine Problem")));
+      log.info("Preloading {}", vehicleServicesRepository.save(new VehicleServices("1", new Date(1646311082),"Customer","Customer","Customer")));
+      log.info("Preloading {}", vehicleTypeRepository.save(new VehicleType("2BHK","1-Ton")));
+      log.info("Preloading {}", vehicleVendorRepository.save(new VehicleVendor("1","2")));
     };
     
   }
